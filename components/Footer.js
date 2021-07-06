72c3394e5b44b2089e5c54174ce8d9d7c5f06a37
HeryaniH <86170048+HeryaniH@users.noreply.github.com>
2021-07-06 09:04:15 +0700

    tinggal part6

---
+++ components/Footer.js
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <div>Copyright 2021 Heryani Gallery List</div>;
+};
+
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Welcome to My Personal Web Site.</p>
+      <p>Welcome to My Personal Web Site.</p>
+    </div>
+  );
+};
+
+export default About;
